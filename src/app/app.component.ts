0a65883b893278376ee4e44adedbf33051cf0480
Omar Nabolsi <on09@aub.edu.lb>
2019-02-04 17:27:11 +0200

    Section 5 Lecture 91

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { UIService } from "./shared/ui.service";
+import { Subscription } from "rxjs";
 
 @Component({
-    selector: "ns-app",
-    moduleId: module.id,
-    templateUrl: "./app.component.html",
+  selector: "ns-app",
+  moduleId: module.id,
+  templateUrl: "./app.component.html"
 })
-export class AppComponent {
-    activeChallenge: string = '';
+export class AppComponent implements OnInit, OnDestroy {
+  activeChallenge: string = "";
+  private drawerSub: Subscription;
 
-    onChallengeInput(challengeDescription: string) {
-        this.activeChallenge = challengeDescription;
-        console.log(challengeDescription);
+  constructor(private uiService: UIService) {}
+
+  ngOnInit() {
+    this.drawerSub = this.uiService.drawerState.subscribe(() => {
+      console.log("Toggle side drawer!");
+    });
+  }
+
+  onChallengeInput(challengeDescription: string) {
+    this.activeChallenge = challengeDescription;
+    console.log(challengeDescription);
+  }
+
+  ngOnDestroy() {
+    if (this.drawerSub) {
+      this.drawerSub.unsubscribe();
     }
- }
+  }
+}
--- src/app/shared/ui/action-bar/action-bar.component.ts
+++ src/app/shared/ui/action-bar/action-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from "@angular/core";
 import { Page } from "tns-core-modules/ui/page";
 import { isAndroid } from "platform";
 import { RouterExtensions } from "nativescript-angular";
+import { UIService } from "../../ui.service";
 
 declare var android: any;
 
@@ -12,29 +13,41 @@ declare var android: any;
   moduleId: module.id
 })
 export class ActionBarComponent {
-    @Input() title: string;
-    @Input() showBackButton = true;
+  @Input() title: string;
+  @Input() showBackButton = true;
 
-    constructor(private page: Page, private router: RouterExtensions) {}
+  constructor(
+    private page: Page,
+    private router: RouterExtensions,
+    private uiService: UIService
+  ) {}
 
-    get canGoBack() {
-        return this.router.canGoBack() && this.showBackButton;
+  get android() {
+    return isAndroid;
+  }
+
+  get canGoBack() {
+    return this.router.canGoBack() && this.showBackButton;
+  }
+
+  onGoBack() {
+    this.router.backToPreviousPage();
+  }
+
+  onLoadedActionBar() {
+    if (isAndroid) {
+      const androidToolbar = this.page.actionBar.nativeView;
+      const backButton = androidToolbar.getNavigationIcon();
+      if (backButton) {
+        backButton.setColorFilter(
+          android.graphics.Color.parseColor("#171717"),
+          (<any>android.graphics).PorterDuff.Mode.SRC_ATOP
+        );
+      }
     }
+  }
 
-    onGoBack() {
-        this.router.backToPreviousPage();
-    }
-
-    onLoadedActionBar() {
-        if (isAndroid) {
-            const androidToolbar = this.page.actionBar.nativeView;
-            const backButton = androidToolbar.getNavigationIcon();
-            if (backButton) {
-            backButton.setColorFilter(
-                android.graphics.Color.parseColor("#171717"),
-                (<any>android.graphics).PorterDuff.Mode.SRC_ATOP
-            );
-            }
-        }
-    }
+  onToggleMenu() {
+      this.uiService.toggleDrawer();
+  }
 }
